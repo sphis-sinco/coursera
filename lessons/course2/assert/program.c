caa2a166e70f22ccbdd56d469c36d01c46f579ac
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 13:25:38 -0500

    division program

--- lessons/course2/assert/program.c
+++ lessons/course2/assert/program.c
@@ -5,8 +5,15 @@
 int main()
 {
         // assert(0); // fail
-        assert(1); // succeed
-        printf("Running program\n");
+        // assert(1); // succeed
+        double x, y;
+        while(1)
+        {
+                printf("Read in 2 floats:\n");
+                scanf("%lf %lf", &x, &y);
+                assert(y != 0);
+                printf("%lf/%lf = %lf\n", x, y, x/y);
+        }
 
         return 0;
 }
