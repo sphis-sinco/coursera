c75a1d3ccecb49dd9eb3dda8c06dfa21cea61ce3
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 07:40:33 -0500

    pointers proper

--- lessons/pointers/program.c
+++ lessons/pointers/program.c
@@ -1,8 +1,23 @@
+#include <stdio.h>
+
 int main(void)
 {
-        int int_array[11] = {0,1,2,3,4,5,6,7,8,9,10};
-        char string[11] = "Hello";
-        char char_array[5] = {'a','b','c','d','e'};
+        const int SIZE = 5;
+        int grades[5] = {78, 67, 92, 83, 88};
+        double sum = 0.0;
+        double *ptr_to_sum = &sum;
+        int i;
+
+        printf("\nMy grades are:\n");
+        for (i = 0; i < SIZE; i++)
+                printf("%d\t", grades[i]);
+        printf("\n\n");
+
+        for (i = 0; i < SIZE; i++)
+                sum = sum + grades[i];
+        printf("My average is %.2f\n\n", sum / SIZE);
+        printf("sum is at %p, or %lu and is %lf\n", ptr_to_sum, ptr_to_sum, *ptr_to_sum);
+        printf("grades are at %lu to %lu\n", grades, grades + 5);
 
         return 0;
 }
