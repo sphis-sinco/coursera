ed286200742a510ef4334629b0119565a9cc6c71
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-28 20:02:50 -0500

    matarthon maker

--- lessons/customMarathon/program.c
+++ lessons/customMarathon/program.c
@@ -2,11 +2,18 @@
 
 int main(void)
 {
-        int miles = 26, yards = 385;
+        int miles = 26, yards = 385;        
+
+        printf("Please enter the miles as an integer:");
+        scanf("%d", &miles);       
+
+        printf("Please enter the yards as an integer:");
+        scanf("%d", &yards);
+
         double kilometers;
 
         kilometers = 1.609 * (miles + yards / 1760.0);
-        printf("\nA marathon is %lf kilometers\n\n", kilometers);
+        printf("\nYour marathon is %lf kilometers\n\n", kilometers);
 
         return 0;
 }
