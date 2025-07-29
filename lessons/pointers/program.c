ee8faa129624399a4b0141eb4452ea6393d96aa8
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 07:25:48 -0500

    string and chars

--- lessons/pointers/program.c
+++ lessons/pointers/program.c
@@ -1,6 +1,8 @@
 int main(void)
 {
-        int array[11] = {0,1,2,3,4,5,6,7,8,9,10};
+        int int_array[11] = {0,1,2,3,4,5,6,7,8,9,10};
+        char string[11] = "Hello";
+        char char_array[5] = {'a','b','c','d','e'};
 
         return 0;
 }
