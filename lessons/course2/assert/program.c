a97912acf388be01726a9d3a2f49572429d3d07b
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-29 13:21:26 -0500

    assert

---
+++ lessons/course2/assert/program.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <assert.h>
+
+int main()
+{
+        // assert(0); // fail
+        assert(1); // succeed
+        printf("Running program\n");
+
+        return 0;
+}
