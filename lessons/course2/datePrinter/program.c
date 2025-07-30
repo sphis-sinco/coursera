1c80576a2bbe898331e9ace1cbe63559f8653058
Sphis-Sinco <sinconsistencia@gmail.com>
2025-07-30 12:37:02 -0500

    date thing

---
+++ lessons/course2/datePrinter/program.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+
+typedef enum month
+{
+        january,
+        febuary,
+        march,
+        april,
+        may,
+        june,
+        july,
+        august,
+        september,
+        october,
+        november,
+        december
+} month;
+typedef struct date
+{
+        month m;
+        int d
+} date;
+
+void print_date(month m, int d)
+{
+        switch (m)
+        {
+        case january:
+                printf("January");
+                break;
+        case febuary:
+                printf("Febuary");
+                break;
+        case march:
+                printf("March");
+                break;
+        case april:
+                printf("April");
+                break;
+        case may:
+                printf("May");
+                break;
+        case june:
+                printf("June");
+                break;
+        case july:
+                printf("July");
+                break;
+        case august:
+                printf("August");
+                break;
+        case september:
+                printf("September");
+                break;
+        case october:
+                printf("October");
+                break;
+        case november:
+                printf("November");
+                break;
+        case december:
+                printf("December");
+                break;
+
+        default:
+                printf("%d", m);
+        }
+        printf(" %lf", d);
+}
+
+int main(void)
+{
+        date thedate = {
+            january,
+            15};
+        print_date(thedate);
+
+        return 0;
+}
